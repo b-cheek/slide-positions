fcb9cd135ad9ff845d154b7c9693bc6c85e1765c
Brayden Cheek <cheekbw@gmail.com>
2026-04-15 20:42:47 -0400

    Prettier setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import "./App.css"
+import "./App.css";
 
 function App() {
-
   return (
     <div>
       <h1 class="text-3xl font-bold underline">Hello World</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
