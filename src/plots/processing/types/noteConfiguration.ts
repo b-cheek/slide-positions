a81e81995599a83860f6f2eb0c770b4a25adf147
Brayden Cheek <cheekbw@gmail.com>
2026-04-18 17:41:37 -0400

    NoteConfiguration class; moving some of the helpers elsewhere

---
+++ src/plots/processing/types/noteConfiguration.ts
@@ -0,0 +1,21 @@
+import { Hertz, Meters } from "./constants";
+import { Tuning } from "./tuning";
+
+export class NoteConfiguration {
+  public readonly tuning: Tuning;
+  public readonly slideDistance: Meters;
+  public readonly partial: number;
+  public readonly lip_bend_hz: Hertz;
+
+  public constructor(
+    tuning: Tuning,
+    slideDistance: Meters,
+    partial: number,
+    lip_bend_hz: Hertz,
+  ) {
+    this.tuning = tuning;
+    this.slideDistance = slideDistance;
+    this.partial = partial;
+    this.lip_bend_hz = lip_bend_hz;
+  }
+}
