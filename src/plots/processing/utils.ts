d76de0d1e1e3d0e827dc80c9951ee40f9afd33cf
Brayden Cheek <cheekbw@gmail.com>
2026-04-18 22:38:57 -0400

    getOpenPos fn

--- src/plots/processing/utils.ts
+++ src/plots/processing/utils.ts
@@ -1,7 +1,13 @@
 import { SPEED_OF_SOUND } from "./types/constants";
-import type { Hertz, Meters, RelativePosition } from "./types/constants";
+import type {
+  AbsolutePosition,
+  Hertz,
+  Meters,
+  RelativePosition,
+} from "./types/constants";
 import { NoteConfiguration } from "./types/noteConfiguration";
 import { Player } from "./types/player";
+import { Trombone } from "./types/trombone";
 
 export function freqToLength(freq: Hertz): Meters {
   // in a tube open at both ends (trombone)
@@ -24,4 +30,18 @@ export function getSlidePosition(
   ) *
     12 +
     1) as RelativePosition;
+}
+
+export function getOpenPosition(
+  player: Player,
+  trombone: Trombone,
+  noteConfig: NoteConfiguration,
+): AbsolutePosition {
+  const openLen = trombone.tunings[0].length;
+  return (Math.log2(
+    (openLen + noteConfig.slideDistance) /
+      (openLen + player.first_pos_distance),
+  ) *
+    12 +
+    1) as AbsolutePosition;
 }
