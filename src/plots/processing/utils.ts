880eca48da2aba2b2a1f22149edecbe37765c6b2
Brayden Cheek <cheekbw@gmail.com>
2026-04-19 12:39:41 -0400

    Remove unused imports

--- src/plots/processing/utils.ts
+++ src/plots/processing/utils.ts
@@ -1,13 +1,5 @@
 import { SPEED_OF_SOUND } from "./types/constants";
-import type {
-  AbsolutePosition,
-  Hertz,
-  Meters,
-  RelativePosition,
-} from "./types/constants";
-import { NoteConfiguration } from "./types/noteConfiguration";
-import { Player } from "./types/player";
-import { Trombone } from "./types/trombone";
+import type { Hertz, Meters } from "./types/constants";
 
 export function freqToLength(freq: Hertz): Meters {
   // in a tube open at both ends (trombone)
