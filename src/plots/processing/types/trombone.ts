09f4ef0366c4f8b449afe9b5768a52f99db352e9
Brayden Cheek <cheekbw@gmail.com>
2026-04-20 21:06:51 -0400

    Fix getnoteconfigs to work with multiple partials

--- src/plots/processing/types/trombone.ts
+++ src/plots/processing/types/trombone.ts
@@ -23,41 +23,59 @@ export class Trombone {
 
   public getNoteConfigs(note: Note, player: Player): NoteConfiguration[] {
     return this.tunings.flatMap((tuning) => {
-      // Flat map allows us to filter out bad configs as we go
-      // by returning [] which is flattened out of the result
-      const openFundamental = lengthToFreq(tuning.length);
-      const partial = Math.ceil(note.freq / openFundamental);
-
-      const targetFundamental = (note.freq / partial) as Hertz;
-      const requiredSlideDistance = (freqToLength(targetFundamental) -
-        tuning.length) as Meters;
-
-      if (requiredSlideDistance <= this.slideLength) {
-        return [
-          new NoteConfiguration(
-            tuning,
-            requiredSlideDistance,
-            partial,
-            0 as Cents,
-          ),
-        ];
-      }
-
-      const maxSlideFundamental = lengthToFreq(
+      const minFreq = lengthToFreq(
         (tuning.length + this.slideLength) as Meters,
       );
-      const maxSlideNoteFreq = (maxSlideFundamental * partial) as Hertz;
-      const requiredLipBendHz = (maxSlideNoteFreq - note.freq) as Hertz;
+      const maxFreq = lengthToFreq(tuning.length);
+      const minPartial = Math.ceil(note.freq / maxFreq);
+      const maxPartial = Math.floor(note.freq / minFreq);
 
-      if (requiredLipBendHz > player.lipBendRange) {
+      if (maxPartial < minPartial) {
         return [];
       }
 
-      const lipBendCents = (1200 *
-        Math.log2(maxSlideNoteFreq / note.freq)) as Cents;
-      return [
-        new NoteConfiguration(tuning, this.slideLength, partial, lipBendCents),
-      ];
+      const partials = Array.from(
+        { length: maxPartial - minPartial + 1 },
+        (_, i) => i + minPartial,
+      );
+
+      return partials.flatMap((partial) => {
+        const targetFundamental = (note.freq / partial) as Hertz;
+        const requiredSlideDistance = (freqToLength(targetFundamental) -
+          tuning.length) as Meters;
+
+        if (requiredSlideDistance <= this.slideLength) {
+          return [
+            new NoteConfiguration(
+              tuning,
+              requiredSlideDistance,
+              partial,
+              0 as Cents,
+            ),
+          ];
+        }
+
+        const maxSlideFundamental = lengthToFreq(
+          (tuning.length + this.slideLength) as Meters,
+        );
+        const maxSlideNoteFreq = (maxSlideFundamental * partial) as Hertz;
+        const requiredLipBendHz = (maxSlideNoteFreq - note.freq) as Hertz;
+
+        if (requiredLipBendHz > player.lipBendRange) {
+          return [];
+        }
+
+        const lipBendCents = (1200 *
+          Math.log2(maxSlideNoteFreq / note.freq)) as Cents;
+        return [
+          new NoteConfiguration(
+            tuning,
+            this.slideLength,
+            partial,
+            lipBendCents,
+          ),
+        ];
+      });
     });
   }
 }
