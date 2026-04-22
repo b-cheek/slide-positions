d9a3f7ae86b26784ed12b920ae13923b40b75c2c
Brayden Cheek <cheekbw@gmail.com>
2026-04-21 22:16:45 -0400

    Fix optional parsing of octave and adjustment, move some math down to Note class

--- src/plots/processing/types/tuning.ts
+++ src/plots/processing/types/tuning.ts
@@ -1,5 +1,5 @@
 import { Note } from "./note";
-import type { Meters, Semitones } from "./constants";
+import type { Meters, Semitones, Cents } from "./constants";
 
 import { freqToLength } from "../utils";
 
@@ -14,23 +14,28 @@ export class Tuning {
     this.name = name;
   }
 
+  // TODO: rename since can take true pitch
   public static fromPitchClass(pitchClass: string): Tuning {
     const match = pitchClass.match(/(\d*)([+-]\d+\.?\d*)?$/);
 
     let octave = "1";
-    let adjustment = 0 as Semitones;
+    let adjustment = "+0";
     let basePitchClass = pitchClass;
 
     // Check that match exists and is not empty
     if (match && match[0]) {
       basePitchClass = pitchClass.slice(0, match.index);
-      octave = match[1] || "1";
-      adjustment = (match[2] || 0 / 100) as Semitones;
+      if (match[1]) {
+        octave = match[1];
+      }
+      if (match[2]) {
+        adjustment = match[2];
+      }
     }
 
-    const note = Note.fromSciNotation(`${basePitchClass}${octave}`);
-    const length = (freqToLength(note.freq) *
-      2 ** (-adjustment / 12)) as Meters;
+    const length = freqToLength(
+      Note.fromSciNotation(`${basePitchClass}${octave}${adjustment}`).freq,
+    ) as Meters;
 
     return new Tuning(length, `${basePitchClass} tuning`);
   }
