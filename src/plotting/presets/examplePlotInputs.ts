2af777a74deb19e5e3394d78bb39e1c99df7c4ee
Brayden Cheek <cheekbw@gmail.com>
2026-05-08 20:53:21 -0400

    Correct usage and generation of example inputs

--- src/plotting/presets/examplePlotInputs.ts
+++ src/plotting/presets/examplePlotInputs.ts
@@ -1,6 +1,10 @@
 import type { RawPlotInputs } from "../parsing/plotInputsSchema";
 import { plotInputsRawSchema } from "../parsing/plotInputsSchema";
 
+export const placeholderInputs: RawPlotInputs = plotInputsRawSchema.parse({
+  notesString: "Bb1 C2 D2",
+} satisfies Partial<RawPlotInputs>);
+
 export const exampleInputs: RawPlotInputs = plotInputsRawSchema.parse({
   notesString: "Bb2 C3 D3 Eb3 F3 G3 A3 Bb3",
-});
+} satisfies Partial<RawPlotInputs>);
